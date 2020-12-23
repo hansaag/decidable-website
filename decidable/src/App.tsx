dd16d65878925a1c74f7eb89f2a46db643793446
Hans Gunleiksrud <hansag91@gmail.com>
2020-12-23 18:26:44 +0100

    added small animation

---
+++ decidable/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './decidable_img.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="header">
+        <div className="topnav">
+          <div className="prosjekter">
+            <h4>Prosjekter</h4>
+          </div>
+          <div className="planer">
+            <h4>Planer</h4>
+          </div>
+          <div className="kontakt">
+           <h4 >Kontakt oss</h4>
+          </div>
+          <div className="logo" id="logo">
+
+        </div>
+        </div>
+        
+        
+      </header> 
+      
+      
+    </div>
+  );
+}
+
+export default App;
